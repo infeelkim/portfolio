fdc8160a578a7252ae0184c3750bcc3fd4cbc6c0
infeelkim <infeelkim@gmail.com>
2022-03-23 02:07:16 +0900

    첫번째

--- js/main.js
+++ js/main.js
@@ -8,3 +8,75 @@ btnCall.onclick = function(e){
     btnCall.classList.toggle("on")
     menuMo.classList.toggle("on")
 }
+
+const outline = document.querySelector("#outline");
+const hoverBox = document.querySelectorAll("#outline li");
+const _top = outline.querySelector(".top");
+const _right = outline.querySelector(".right");
+const _left = outline.querySelector(".left");
+const _bottom = outline.querySelector(".bottom");
+const _inner = outline.querySelector(".inner");
+const speed = 200;
+console.log(hoverBox);
+
+for(let el of hoverBox){
+    el.addEventListener("mouseenter", ()=>{
+        
+        console.log("in");
+
+        new Anime(_top,{
+            prop: "width",
+            value: "100%",
+            duration: speed,
+            callback:()=>{
+                new Anime(_right,{
+                    prop: "height",
+                    value: "100%",
+                    duration: speed,
+                    callback:()=>{
+                        new Anime(_bottom,{
+                            prop: "width",
+                            value: "100%",
+                            duration: speed,
+                            callback:()=>{
+                                new Anime(_left,{
+                                    prop: "height",
+                                    value: "100%",
+                                    duration: speed,
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    });
+
+
+
+    el.addEventListener("mouseleave", ()=>{
+
+        console.log("out");
+
+        new Anime(_top,{
+            prop:"width", 
+            value:0, 
+            duration:speed
+        });
+        new Anime(_right,{
+            prop:"height", 
+            value:0, 
+            duration:speed
+        })
+        new Anime(_bottom,{
+            prop:"width", 
+            value:0, 
+            duration:speed
+        })
+        new Anime(_left,{
+            prop:"height", 
+            value:0, 
+            duration:speed,
+        })
+    }) 
+}
